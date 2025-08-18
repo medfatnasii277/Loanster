40a507742b4d0c8ed40987f909ced5054b73ba8c
medfatnasii277 <mohamedfatnassi277@gmail.com>
2025-08-18 11:51:26 +0100

    adding borrower info in the detaied loan app - officer veiw

--- officer-service/src/main/java/com/pm/officerservice/dto/LoanApplicationResponse.java
+++ officer-service/src/main/java/com/pm/officerservice/dto/LoanApplicationResponse.java
@@ -3,6 +3,7 @@ package com.pm.officerservice.dto;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import com.pm.officerservice.model.Borrower;
 import com.pm.officerservice.model.LoanApplication;
 import com.pm.officerservice.model.LoanApplicationStatus;
 
@@ -20,6 +21,10 @@ public class LoanApplicationResponse {
     private Long applicationId;
     private Long borrowerId;
     private String borrowerName;
+    private String borrowerEmail;
+    private String borrowerPhoneNumber;
+    private Double borrowerAnnualIncome;
+    private String borrowerEmploymentStatus;
     private BigDecimal loanAmount;
     private Integer loanTermMonths;
     private String loanPurpose;
@@ -33,21 +38,26 @@ public class LoanApplicationResponse {
     private LocalDateTime updatedAt;
     
     public static LoanApplicationResponse fromEntity(LoanApplication entity) {
-        return LoanApplicationResponse.builder()
-                .applicationId(entity.getApplicationId())
-                .borrowerId(entity.getBorrower().getBorrowerId())
-                .borrowerName(entity.getBorrower().getFirstName() + " " + entity.getBorrower().getLastName())
-                .loanAmount(entity.getLoanAmount())
-                .loanTermMonths(entity.getLoanTermMonths())
-                .loanPurpose(entity.getLoanPurpose())
-                .interestRate(entity.getInterestRate())
-                .monthlyPayment(entity.getMonthlyPayment())
-                .status(LoanApplicationStatus.valueOf(entity.getStatus()))
-                .statusUpdatedBy(entity.getStatusUpdatedBy())
-                .statusUpdatedAt(entity.getStatusUpdatedAt())
-                .appliedAtSource(entity.getAppliedAtSource())
-                .createdAt(entity.getCreatedAt())
-                .updatedAt(entity.getUpdatedAt())
-                .build();
+    Borrower borrower = entity.getBorrower();
+    return LoanApplicationResponse.builder()
+        .applicationId(entity.getApplicationId())
+        .borrowerId(borrower.getBorrowerId())
+        .borrowerName(borrower.getFirstName() + " " + borrower.getLastName())
+        .borrowerEmail(borrower.getEmail())
+        .borrowerPhoneNumber(borrower.getPhoneNumber())
+        .borrowerAnnualIncome(borrower.getAnnualIncome())
+        .borrowerEmploymentStatus(borrower.getEmploymentStatus())
+        .loanAmount(entity.getLoanAmount())
+        .loanTermMonths(entity.getLoanTermMonths())
+        .loanPurpose(entity.getLoanPurpose())
+        .interestRate(entity.getInterestRate())
+        .monthlyPayment(entity.getMonthlyPayment())
+        .status(LoanApplicationStatus.valueOf(entity.getStatus()))
+        .statusUpdatedBy(entity.getStatusUpdatedBy())
+        .statusUpdatedAt(entity.getStatusUpdatedAt())
+        .appliedAtSource(entity.getAppliedAtSource())
+        .createdAt(entity.getCreatedAt())
+        .updatedAt(entity.getUpdatedAt())
+        .build();
     }
 } 
